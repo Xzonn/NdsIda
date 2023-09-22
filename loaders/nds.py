f734e8e3dd8854a540a42feb1c15990719293639
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2023-09-22 18:35:43 +0800

    Should support Py3.8-3.9

--- loaders/nds.py
+++ loaders/nds.py
@@ -10,7 +10,7 @@ if sys.version_info[0] != 3 or sys.version_info[1] < 10:
     Exception("Please update your python installation to 3.10.x or higher.")
 script_name = os.path.basename(__file__)
 script_path = os.path.realpath(__file__)
-script_path = script_path.removesuffix(script_name) + "../nds"
+script_path = os.path.dirname(os.path.dirname(script_path)) + "/nds"
 sys.path.insert(0, script_path)
 #
 
